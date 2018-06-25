c9c25f41012408981a8ed1cf9a21d47b31d00f39
Your Name <you@example.com>
2018-06-25 15:53:28 +0800

    前/后台

---
+++ 0615前,后台/mvc/Controller/admin/Charts.php
@@ -0,0 +1,31 @@
+<?php 
+namespace Controller\admin;
+use Controller\Controller;
+use Model\Db;
+class Charts extends Controller
+{
+    public function charts_1()
+    {
+        $this->display('admin/Charts/charts_1');
+    }
+    public function charts_2()
+    {
+        $this->display('admin/Charts/charts_2');
+    }
+    public function charts_3()
+    {
+        $this->display('admin/Charts/charts_3');
+    }
+    public function charts_4()
+    {
+        $this->display('admin/Charts/charts_4');
+    }
+    public function charts_5()
+    {
+        $this->display('admin/Charts/charts_5');
+    }
+    public function charts_6()
+    {
+        $this->display('admin/Charts/charts_6');
+    }
+}
---
+++ 0615前,后台/mvc/view/index/index.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<!-- saved from url=(0021)http://www.yzipi.com/ -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>
+柚段子 _ 有故事，有内涵！</title>
+
+<meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=no">
+<meta name="description" content="有思维,有深度,有内涵的互联网段子！">
+<meta name="keywords" content="柚子皮,段子,营销段子,内涵段子,搞笑段子">
+<link rel="shortcut icon" href="http://www.yzipi.com/wp-content/themes/yzipi/images/favicon.ico">
+<link rel="stylesheet" id="light-css" href="./Public/home/index_files/light.css" type="text/css" media="all">
+<link rel="stylesheet" id="sytle-css" href="./Public/home/index_files/style.css" type="text/css" media="all">
+<script src="./Public/home/index_files/hm.js"></script><script type="text/javascript" src="./Public/home/index_files/html5shiv.js"></script>
+<script type="text/javascript" src="./Public/home/index_files/selectivizr-min.js"></script>
+<script type="text/javascript" src="./Public/home/index_files/jquery.min.js"></script>
+<script type="text/javascript" src="./Public/home/index_files/jiazai.js"></script>
+<script type="text/javascript" src="./Public/home/index_files/swiper.min.js"></script>
+</head>
+<body>
+<header class="header-web">
+  <nav>
+   <a href="index.php?c=user&a=index" class="logo" title="柚段子" rel="home"><img src="./Public/home/index_files/logo.png" alt="柚段子Logo"></a>
+   <form method="get" class="search" action="index.php?c=user&a=seek">
+      <input class="text" type="text" name="s" placeholder="Search" value="">   
+    </form>
+    <ul class="nav-list">
+        <li id="menu-item-1296" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-1296" style="width:95px;"><a href="index.php?c=admin\index&a=index">后台登录</a></li>
+        <li id="menu-item-1296" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-1296"><a href="index.php?a=cargo">干货</a></li>
+        <li id="menu-item-1295" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-1295"><a href="index.php?a=cross">段子</a></li>
+        <li id="menu-item-127" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-127"><a href="index.php?a=details">D1.7</a></li>
+        <li id="menu-item-1357" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1357"><a href="index.php?a=regards">关于</a></li>
+    </ul>   
+  </nav>
+</header>
+<!--header-web--><div id="container-page">
+ 
+  <article class="box">
+  
+   <!--幻灯片-->
+<div class="hmFocus">
+<div class="swiper-container autoImg swiper-container-horizontal">
+    <div class="swiper-wrapper" style="transition-duration: 0ms; transform: translate3d(-1600px, 0px, 0px);"><div class="swiper-slide swiper-slide-duplicate" data-swiper-slide-index="1" style="width: 800px;">  <a href="http://www.yzipi.com/388.htm" target="_blank"><img src="./Public/index_files/huan2.png" alt=""></a></div>
+        <div class="swiper-slide swiper-slide-prev" data-swiper-slide-index="0" style="width: 800px;"> <a href="http://www.yzipi.com/133.htm" target="_blank"><img src="./Public/home/index_files/huan1.png" 　alt=""></a></div>
+              <div class="swiper-slide swiper-slide-active" data-swiper-slide-index="1" style="width: 800px;">  <a href="http://www.yzipi.com/388.htm" target="_blank"><img src="./Public/index_files/huan2.png" alt=""></a></div>
+    <div class="swiper-slide swiper-slide-duplicate swiper-slide-next" data-swiper-slide-index="0" style="width: 800px;"> <a href="http://www.yzipi.com/133.htm" target="_blank"><img src="./Public/home/index_files/huan1.png" 　alt=""></a></div></div>
+    <div class="swiper-pagination swiper-pagination-clickable"><span class="swiper-pagination-bullet"></span><span class="swiper-pagination-bullet swiper-pagination-bullet-active"></span></div>
+</div>
+</div>
+<script language="javascript">
+var swiper = new Swiper('.hmFocus .swiper-container', {
+    pagination: '.swiper-pagination',
+    loop: true,
+    autoplay: 5500,
+    paginationClickable: true
+});
+</script>
+  
+    <?php foreach ($data as $v) { ?>        
+   <section class="list">
+       <span class="titleimg"> <a href="index.php?c=user&a=details&id=<?php echo $v['id'] ?>" target="_blank">
+  <img width="1000" height="556" src="./Public/home/index_files/lo-1000x556.jpg" class="attachment-thumbnail size-thumbnail wp-post-image" alt="" srcset="http://www.yzipi.com/wp-content/uploads/2018/03/lo-1000x556.jpg 1000w, http://www.yzipi.com/wp-content/uploads/2018/03/lo-768x427.jpg 768w" sizes="(max-width: 1000px) 100vw, 1000px">  </a> </span>
+    
+  <div class=" mecc">
+     
+    <h2 class="mecctitle">
+    <a href="index.php?c=user&a=details&id=<?php echo $v['id'] ?>" target="_blank" id="<?php echo $v['id'] ?>">
+    <?php echo $v['title'] ?></a> </h2>
+    <time><?php echo $v['start'] ?></time>
+  </div>
+
+  <div class=" zaiyao">
+    <p><?php echo $v['act'] ?></p>
+  </div>   
+</section>
+<?php } ?>
+<div class="posts-nav">
+<span aria-current="page" class="page-numbers current">1</span>
+<a class="page-numbers" href="http://www.yzipi.com/page/2">2</a>
+<a class="page-numbers" href="http://www.yzipi.com/page/3">3</a>
+<a class="page-numbers" href="http://www.yzipi.com/page/4">4</a></div>
+ </article>   
+
+ 
+  <div id="post-read-more" style="display: none;">  
+    <a href="http://www.yzipi.com/page/2">看更多</a>  
+</div>  
+<style>#post-read-more{display:block;}</style>
+ <script type="text/javascript"> 
+    <!-- 
+            //平台、设备和操作系统 
+            var system = { 
+                win: false, 
+                mac: false, 
+                xll: false, 
+                ipad:false 
+            }; 
+            //检测平台 
+            var p = navigator.platform; 
+            system.win = p.indexOf("Win") == 0; 
+            system.mac = p.indexOf("Mac") == 0; 
+            system.x11 = (p == "X11") || (p.indexOf("Linux") == 0); 
+            system.ipad = (navigator.userAgent.match(/iPad/i) != null)?true:false; 
+            //跳转语句
+            if (system.win || system.mac || system.xll||system.ipad) { 
+           
+       document.getElementById("post-read-more").style.display="none";  
+            } else { 
+     
+
+            } 
+    --> 
+    </script> 
+  <aside id="sitebar">
+<div class="sitebar_list">
+             <h4 class="sitebar_title">随机原创</h4>    
+             <ul>  
+  
+    <li>  
+        <a href="http://www.yzipi.com/1162.htm">是骡子还是马，拉出来溜溜就知道</a> 
+   
+    </li>  
+  
+    <li>  
+        <a href="http://www.yzipi.com/878.htm">我为什么要装逼，请看好第四点</a> 
+   
+    </li>  
+  
+    <li>  
+        <a href="http://www.yzipi.com/747.htm">世界互联网颁奖晚会，我们获奖了…</a> 
+   
+    </li>  
+  
+    <li>  
+        <a href="http://www.yzipi.com/1050.htm">老板每天都在给员工画饼，而我却消化不良</a> 
+   
+    </li>  
+  
+    <li>  
+        <a href="http://www.yzipi.com/1232.htm">假如你的人生能有100岁，你活了已经超过1/4</a> 
+   
+    </li>  
+  
+</ul>  
+</div>   
+ <div class="sitebar_list">
+      <a href="http://www.yzipi.com/gardenl" target="_blank"><img width="699" height="341" src="./Public/Home/index_files/S.jpg" class="image wp-image-1158  attachment-full size-full" alt="" style="max-width: 100%; height: auto;"></a></div> <div class="sitebar_list">
+       <h4 class="sitebar_title">网站标签</h4><div class="tagcloud">
+        <a href="#" class="tag-cloud-link tag-link-6 tag-link-position-6" style="font-size: 15px;">此处热搜，不会不做研究</a>
+</div>
+</div><a href="http://www.yzipi.com/service" target="_blank"><img class="totop" src="./Public/home/index_files/GE.jpg"></a>
+   <script type="text/javascript"> 
+$(function() { 
+    var elm = $('.totop'); 
+    var startPos = $(elm).offset().top; 
+    $.event.add(window, "scroll", function() { 
+        var p = $(window).scrollTop(); 
+        $(elm).css('position',((p) > startPos) ? 'fixed' : 'static'); 
+        $(elm).css('top',((p) > startPos) ? '0px' : ''); 
+    }); 
+}); 
+</script>
+</aside></div>
+<footer id="dibu">
+  <div class="dibu-main">
+      <div class="bottomlist">
+      <div class="xinlan"> <a href="https://weibo.com/pocoe" target="_blank"><img src="./Public/index_files/xinlan.png" alt="新浪微博" title="新浪微博"></a> </div>
+      <div class="weixin2">
+        <div class="weixin"><span>公众号 : Yzipi6</span> <img src="./Public/home/index_files/weixin.png" alt="微信公众号" title="微信公众号"> <img src="./Public/home/index_files/er2.png" class="xixii" alt="微信公众号"></div>
+      </div>
+    </div>
+    
+       <div class="bottom">
+       <ul class="botop">     <li id="menu-item-387" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-387"><a href="http://www.yzipi.com/service">网站服务</a></li>
+<li id="menu-item-708" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-708"><a href="http://www.yzipi.com/biao">网站标识</a></li>
+<li id="menu-item-29" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-29"><a href="http://www.yzipi.com/mian">免责声明</a></li>
+<li id="menu-item-1194" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1194"><a href="http://www.yzipi.com/about">关于我们</a></li>
+</ul>
+       
+       <div class="botext"><a href="http://www.yzipi.com/">
+        柚段子        </a> , 
+       有故事，有内涵！  <a href="http://www.miitbeian.gov.cn/" rel="external nofollow" target="_blank"> 粤ICP备18005985号 </a></div>
+        <div class="tongji"><script> var _hmt = _hmt || []; (function() {   var hm = document.createElement("script");   hm.src = "https://hm.baidu.com/hm.js?74b4edd4d9170ff3878a0902e06b4c08";   var s = document.getElementsByTagName("script")[0];    s.parentNode.insertBefore(hm, s); })(); </script></div>
+    </div>
+  </div>
+  
+    <div class="off">
+  <div class="scroll" id="scroll" style="display:none;"> ︿ </div>
+  </div>
+  <script type="text/javascript">
+    $(function(){
+        showScroll();
+        function showScroll(){
+            $(window).scroll( function() { 
+                var scrollValue=$(window).scrollTop();
+                scrollValue > 500 ? $('div[class=scroll]').fadeIn():$('div[class=scroll]').fadeOut();
+            } );    
+            $('#scroll').click(function(){
+                $("html,body").animate({scrollTop:0},1224); 
+            }); 
+        }
+    })
+    </script> 
+</footer>
+<!--dibu-->
+<script type="text/javascript" src="./Public/home/index_files/wp-embed.min.js"></script>
+</body></html>
